0d827c9c229aca937a2d2625931295e126ee5226
tl <tl@gmail.com>
2020-07-10 19:05:04 +0530

    dev1 code changed in utilClass.

--- Project/src/com/wipro/util/UtilClass.java
+++ Project/src/com/wipro/util/UtilClass.java
@@ -5,6 +5,7 @@ import java.util.Date;
 public class UtilClass {
 	public static void main(String[] args) {
 		System.out.println("Current date is "+new Date());
+		System.out.println("new code has been added");
 	}
 
 }
