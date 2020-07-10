86f70d83960ac23cb977dac850fd3e6b00d8564b
tl <tl@gmail.com>
2020-07-10 18:37:40 +0530

    code chnages in util calls

--- Project/src/com/wipro/util/UtilClass.java
+++ Project/src/com/wipro/util/UtilClass.java
@@ -1,5 +1,10 @@
 package com.wipro.util;
 
+import java.util.Date;
+
 public class UtilClass {
+	public static void main(String[] args) {
+		System.out.println("Current date is "+new Date());
+	}
 
 }
